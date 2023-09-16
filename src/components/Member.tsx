31324bf29c3339be63712717e69cd9b08302f519
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2023-09-16 18:39:58 +0700

    add checkbox to filter active member

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
 import data from '../data/member.json'
 
 const Member = () => {
+  const [isActiveMember,setIsActiveMember] = useState(false)
+  const filteredData = isActiveMember ? data.filter(item => item.status === "mem") : data
   return (
 <>
-    {data.map((item, index) => (
+    <label htmlFor="">
+      Active Member <input type="checkbox" name="" onChange={() => setIsActiveMember(!isActiveMember)} />
+    </label>
+    {filteredData.map((item, index) => (
       <div key={index}>
         <h1>{item.nama}</h1>
         <h4>Generasi {item.generasi}</h4>
