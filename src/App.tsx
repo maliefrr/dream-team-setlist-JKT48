a81a97b5dfb5b14f9be9999f74ea596f12c7ff5d
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2023-09-19 13:51:47 +0700

    make table for the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import Member from "./components/Member"
-
+// import Member from "./components/Member"
+import { useState } from 'react';
+import "./style.css"
 
 function App() {
-   
+  const [numPlayers, setNumPlayers] = useState(0);
+  const handleNumPlayersChange = (event) => {
+    const inputValue = event.target.value;
+    setNumPlayers(inputValue);
+  };
+
+  const generateTable = () => {
+    const table = [];
+    for (let i = 0; i <= 16; i++) {
+      const row = [];
+      for (let j = 0; j <= numPlayers; j++) {
+        row.push(<td key={`${i}-${j}`}>{i === 0 && j > 0 ? `Player ${j}` : j === 0 && i > 0 ? `${i}` : i >= 5 && i <= 9 ? <div className='unitsong'></div> : <div className='groupsong'></div>}</td>);
+      }
+      table.push(<tr key={i}>{row}</tr>);
+    }
+    return table;
+  };
   return (
     <>
-    <Member />
+    {/* <Member /> */}
+    <div>
+        <button
+          onClick={() => {
+            const input = prompt('Masukkan jumlah pemain:');
+            const num = parseInt(input, 10);
+            if (!isNaN(num)) {
+              setNumPlayers(num);
+            } else {
+              alert('Jumlah pemain harus berupa angka.');
+            }
+          }}
+        >
+          Masukkan jumlah pemain
+        </button>
+      </div>
+
+      {numPlayers > 0 && (
+        <table className='table'>
+          <tbody>{generateTable()}</tbody>
+        </table>
+      )}
     </>
   )
 }
